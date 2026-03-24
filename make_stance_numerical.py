4362ab4e858136e5b50139b2ddb8113850ee6274
SMOO1 <bbruh188@gmail.com>
2026-03-24 14:43:16 -0400

    remove unnecessary columns

--- make_stance_numerical.py
+++ make_stance_numerical.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("no_quotes_and_percent.csv")
+df = pd.read_csv("cleaned_stance_numeric.csv")
 
-df = pd.get_dummies(df, columns=["Stance"], dtype=int)
-df["Weight"] = df["Weight"].str.replace(' lbs', '')
+#df = pd.get_dummies(df, columns=["Stance"], dtype=int)
+#df["Weight"] = df["Weight"].str.replace(' lbs', '')
+
+
+
+#drop_cols = ["Unnamed: 0.2", "Unnamed: 0.1", "Unnamed: 0"]
+#df = df.drop(drop_cols, axis = 1)
+
+df = df.drop("NaN", index = 1)
 
 print(df)
 df.to_csv("cleaned_stance_numeric.csv")
