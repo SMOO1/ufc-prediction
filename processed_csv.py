602d82bec1604838a6d0289f3da3cfee2c71c732
SMOO1 <bbruh188@gmail.com>
2026-03-24 15:43:42 -0400

    add: gendered dataset

--- processed_csv.py
+++ processed_csv.py
@@ -8,9 +8,20 @@ women = set() #set.update automatically excludes dupes
 women.update(women_fights["winner_name"])
 women.update(women_fights["loser_name"])
 
-
-print(women_fights)
-
+women_list = list(women)
 print(women)
 
-women_fights.to_csv("women_fight_stats.csv")
+
+#print(women_fights)
+
+#print(women)
+
+#women_fights.to_csv("women_fight_stats.csv")
+
+
+final_df = pd.read_csv("cleaned_numeric_ages.csv")
+
+final_df["Female"] = final_df["fighter_name"].apply(lambda x: 1 if x.upper() in women else 0)
+
+print(final_df)
+final_df.to_csv("gendered_stats.csv", index=False)
