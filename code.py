6c5c8b431df0b17f792fefeb0023969ac4432e21
SMOO1 <bbruh188@gmail.com>
2026-03-24 12:01:40 -0400

    feat: remove quotes and percentages fro mdatase, convert percentages to decimal

--- code.py
+++ code.py
@@ -4,9 +4,16 @@ import numpy as np
 cols = ["Height", "Weight", "Reach", "SLpM", "Str_Acc", "SApM", "Str_Def",
     "TD_Avg", "TD_Acc", "TD_Def", "Sub_Avg"]
 
-df = pd.read_csv("raw_fighter_details.csv")
+df = pd.read_csv("zero_removed.csv")
 
-df[cols] = df[cols].replace(0, np.nan)
-df.to_csv("zero_to_nan.csv")
+df["Height"] = df["Height"].str.replace('"', '')
+df["Reach"] = df["Height"].str.replace('"', '')
+
+percent_cols = ["Str_Acc", "Str_Def", "TD_Acc", "TD_Def"]
+
+for col in percent_cols:
+    df[col] = df[col].str.replace('%', '').astype(float) / 100 #convert percentages to decimal
+
+df.to_csv("no_quotes_and_percent.csv")
 print(df)
 
