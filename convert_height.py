91420377583b75383573a13ab4b5d3c75858dd21
SMOO1 <bbruh188@gmail.com>
2026-03-25 14:07:04 -0400

    feat: convert feet to inches

---
+++ convert_height.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+
+def height_to_inches(height):
+    feet, inches = height.split("'")
+    
+    return 12 * int(feet) + int(inches)
+
+df = pd.read_csv("gendered_stats.csv")
+df["Height"] =  df["Height"].apply(lambda x: height_to_inches(x))
+df["Reach"] = df["Reach"].apply(lambda x: height_to_inches(x))
+
+df.to_csv("converted_height.csv")
+
